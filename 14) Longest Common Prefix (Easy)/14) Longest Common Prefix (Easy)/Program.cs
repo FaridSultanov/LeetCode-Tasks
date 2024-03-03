08a4131f3a33e535967425af9c047d5ce3efb9be
FaridSultanov <sultanovvfaridd@gmail.com>
2024-03-03 11:03:11 +0400

    LeetCode

---
+++ 14) Longest Common Prefix (Easy)/14) Longest Common Prefix (Easy)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _14__Longest_Common_Prefix__Easy_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+
+        public static string LongestCommonPrefix(string[]strs)
+        {
+            
+            int count = 0;
+            string value = strs[0];
+            for(int i=0;i<strs.Length;i++)
+            {
+               
+                
+
+            }
+            return "";
+
+        }
+    }
+}
---
+++ 8. String To Integer (Medium)/8. String To Integer (Medium)/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.String_To_Integer__Medium_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int MyAtoi(string s)
+        {
+            return 0;
+        }
+    }
+}
