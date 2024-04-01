e9cc757127523790162061be173050a97f3fcbe6
FaridSultanov <sultanovvfaridd@gmail.com>
2024-04-01 14:10:11 +0400

    Day1

---
+++ 1071. Greatest Common Divisor of Strings/1071. Greatest Common Divisor of Strings/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1071.Greatest_Common_Divisor_of_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ss = "    the sky is blue      ";
+            Console.WriteLine(ReverseWords(ss));
+        }
+
+
+        public  static string ReverseWords(string s)
+        {
+            int left = 0;
+            int right = s.Length - 1;
+
+            
+            while (left <= right && s[left] == ' ')
+            {
+                left++;
+            }
+
+            
+            while (right >= left && s[right] == ' ')
+            {
+                right--;
+            }
+
+            
+            if (left > right)
+            {
+                return "";
+            }
+
+            string ss= s.Substring(left, right - left + 1);
+            string[]array=ss.Split(' ');
+
+            //for(int i=0;i<array.Length; i++)
+            //{
+            //    Console.WriteLine(array[i]);
+            //}
+            string k = "";
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                k += array[i];
+                if(i!=0)
+                {
+                    k+= " ";
+                }
+            }
+            return k;
+        }
+    }
+}
+
---
+++ 58) Length of Last Word/58) Length of Last Word/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _58__Length_of_Last_Word
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "ab  ";
+            Console.WriteLine(LengthofLastWord(s));
+        }
+
+        public static int LengthofLastWord(string s)
+        {
+            int count = 0, index = s.Length - 1;
+            while (count < s.Length)
+            {
+                if (s[index] == ' ')
+                {
+                    index--;
+                }
+                else if (s[index] != ' ')
+                {
+                    count++;
+                    if (index == 0)
+                    {
+                        break;
+                    }
+                    else if (s[index - 1] == ' ' || s[index - 1] == null)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        index--;
+                    }
+
+                }
+                else
+                {
+                    index--;
+                }
+
+
+                // if(index<0)
+                // {
+                //     break;
+                // }
+            }
+            return count;
+        }
+    }
+}
